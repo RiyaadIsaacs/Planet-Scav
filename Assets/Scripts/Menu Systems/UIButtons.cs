652be42be413742e58cbea63fe40e3df2ae79bfe
ZaNekoUvU <angusvaneeden@gmail.com>
2026-03-29 16:21:41 +0200

    Fixed the retry button to reload the scene

--- Assets/Scripts/Menu Systems/UIButtons.cs
+++ Assets/Scripts/Menu Systems/UIButtons.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIButtons : MonoBehaviour
 {
@@ -12,10 +13,14 @@ public class UIButtons : MonoBehaviour
 
     public void retry()
     {
-        player.health = 3;
-        deathHandling.HPGainHandler(); // Restore health visuals
-        deathCanvas.gameObject.SetActive(false); // Hide the death canvas
+        //player.health = 3;
+        //deathHandling.HPGainHandler(); // Restore health visuals
+        //deathCanvas.gameObject.SetActive(false); // Hide the death canvas
         Time.timeScale = 1f; // Resume the game
+        AudioListener.pause = false;
+
+        // Reload the currently active scene to reset everything in that scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void MainMenu()
