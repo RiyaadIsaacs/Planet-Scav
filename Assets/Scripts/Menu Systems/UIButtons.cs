a72f5c3210bcd994be056f2ec5d0c43f0fa68cd5
ZaNekoUvU <angusvaneeden@gmail.com>
2026-03-29 14:44:32 +0200

    deadth handling and ui?

---
+++ Assets/Scripts/Menu Systems/UIButtons.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class UIButtons : MonoBehaviour
+{
+    public DeathHandling deathHandling;
+    public Canvas deathCanvas;
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+
+    public void retry()
+    {
+        deathHandling.health = 3;
+        deathHandling.HPGainHandler(); // Restore health visuals
+        deathCanvas.gameObject.SetActive(false); // Hide the death canvas
+        Time.timeScale = 1f; // Resume the game
+    }
+
+    public void MainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+}
