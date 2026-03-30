369140f49b133b9f006bafadda8facaaa324aad8
ZaNekoUvU <angusvaneeden@gmail.com>
2026-03-30 23:36:25 +0200

    exploraiton pickup done

---
+++ Assets/Scripts/PickUpHandler.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PickUpHandler : MonoBehaviour
+{
+    [Tooltip("Multiplier to apply to the player's charge multiplier when picked up.")]
+    [SerializeField] private float pickupMultiplier = 1.5f;
+
+    [Tooltip("How many charged jumps the multiplier should apply to.")]
+    [SerializeField] private int uses = 5;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other; //.GetComponent<PlayerController>();
+
+        if (CompareTag("PropaneTank"))
+        {
+            if (player != null)
+            {
+                player.GetComponent<PlayerController>().ApplyChargeMultiplier(pickupMultiplier, uses);
+
+                Destroy(gameObject);
+            }
+        }
+
+        if (CompareTag("ExplPickUp"))
+        {
+            player.GetComponent<PlayerStats>().CoinGainHandler();
+
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,8 +1,20 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
+    [SerializeField] public int coinGain = 50;
+
+    public TMP_Text coinText; // Reference to the TextMeshPro component for displaying coins
+
     public int health = 3; // Player's health
     public Transform playerTrans;
     public int coins = 0; // Player's coin count
+
+    public void CoinGainHandler()
+    {
+        coins += coinGain;
+
+        coinText.text = coins.ToString();
+    }
 }
